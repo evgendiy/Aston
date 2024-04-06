2242eb59d6215cc02c7382a9443b132cf64b3f24
Evgeniy Pokatashkin <evgeniypokat@gmail.com>
2024-04-07 00:41:39 +0300

    Added homework Lesson8

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,41 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+
+        String[][] array = new String[4][4];
+        for (String[] strings : array) {
+            Arrays.fill(strings, "1");
+        }
+
+        try {
+            System.out.println("Сумма элементов массива: " + sumArray(array));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static int sumArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        int sum = 0;
+        if (array.length == 4) {
+            for (int i = 0; i < array.length; i++) {
+                if (array[i].length != 4) {
+                    throw new MyArraySizeException("Размер массива не соответствует условию");
+                }
+            }
+        } else {
+            throw new MyArraySizeException("Размер массива не соответствует условию");
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Невалидные данные: ячейка [" + i + "][" + j + "]");
+                }
+            }
+        }
+        return sum;
     }
 }
